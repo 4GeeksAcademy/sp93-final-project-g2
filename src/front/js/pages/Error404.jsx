9e1410bf072a894a9ee7b4dd97227e8b352e8e0f
Héctor Chocobar-Torrejón <45291267+hchocobar@users.noreply.github.com>
2025-03-18 04:02:07 +0000

    [review] fix: code style according best practices

---
+++ src/front/js/pages/Error404.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+
+export const Error404 = () => {
+
+  return (
+    <div className="container">
+      <h1 className="text-danger">Page Not Found</h1>
+    </div>
+  )
+}
